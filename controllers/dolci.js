e72ddaae91dcad18362e37183f88ab57aae7856f
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-05-28 17:19:37 +0200

    Creato Delete e creazione in lavorazione

--- controllers/dolci.js
+++ controllers/dolci.js
@@ -1,6 +1,6 @@
 let dolci = require("../db/db.js");
 const path = require("path");
-const fs = require ("fs")
+const fs = require("fs")
 
 // Index
 const index = (req, res) => {
@@ -51,8 +51,53 @@ const deleteDolce = (req, res) => {
         res.status(404).send({ message: 'Impossibile eliminare il dolce perché non trovato' });
     }
 };
+
+// Create
+// Aggiungere il file
+const updateDolci = (dolci) => {
+    const filePath = path.join(__dirname, "../db/db.js");
+    const content = `module.exports = ${JSON.stringify(dolci, null, 2)};`;
+    fs.writeFileSync(filePath, content, "utf-8");
+};
+
+// Cancellare la foto
+const deletePublicFile = (filename) => {
+    const filePath = path.join(__dirname, "../public", filename);
+    if (fs.existsSync(filePath));
+}
+
+const create = (req, res) => {
+    // Creo il corpo
+    const { title, content, image, tags } = req.body;
+    // Controllo se NON esistono in tal caso ellimino la foto e i dati
+    if (!title || title.replaceAll('/', "").trim().length == 0 || !content || !tags) {
+        req.file?.filename && deletePublicfile(req.file.filename);
+        return res.status(400).send("Mancano alcuni valori")
+        // Controllo se il file esiste e se è una foto
+    } else if (!req.file || !req.file.mimetype.includes("image")) {
+        return res.status(400).send("immagine non inserita o non adatta")
+    }
+    // creo uno slug in base al titolo 
+    const slug = (title) => {
+        const baseSlug = title.replaceAll(' ', '-').toLowerCase().replaceAll('/', '');
+        const slugs = dolci.map(p => p.slug);
+        let counter = 1;
+        let slug = baseSlug;
+        while (slugs.includes(slug)) {
+            slug = `${baseSlug}-${counter}`;
+            counter++;
+        }
+        return slug;
+    }
+
+    updateDolci()
+    res.send(`Il dolce ${title} è stato aggiunto al seguente link http://localhost:3000/dolci/${slug}`)
+
+}
+
 module.exports = {
     index,
     show,
-    deleteDolce
+    deleteDolce,
+    create
 };
--- routers/dolciRouters.js
+++ routers/dolciRouters.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const dolciController = require ("../controllers/dolci.js")
+const dolciController = require("../controllers/dolci.js")
 const multer = require("multer");
-const uploader = multer({dest: "public"});
+const uploader = multer({ dest: "public" });
 
 // mostro tutti
 router.get("/", dolciController.index);
@@ -11,7 +11,7 @@ router.get("/", dolciController.index);
 router.get("/:slug", dolciController.show);
 
 // Post
-// router.post("/", uploader.single("image"),dolciController.create)
+router.post("/", uploader.single("image"), dolciController.create)
 
 // Delete
 router.delete("/:slug", dolciController.deleteDolce)
